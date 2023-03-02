3306d2a9a0891079e4768f829625f2dfba030530
saimum1978 <saimuminfo@gmail.com>
2023-03-03 00:41:53 +0600

    admin

---
+++ app/src/main/java/com/example/saimumbarta/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.saimumbarta;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+        import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+        import android.view.KeyEvent;
+        import android.webkit.WebSettings;
+        import android.webkit.WebView;
+        import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+        webSettings.setSupportZoom(true);
+        webView.loadUrl("https://www.saimumbarta.com");
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                showErrorDialog(description);
+            }
+        });
+    }
+
+    private void showErrorDialog(String message) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Error")
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .show();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
---
+++ app/src/main/java/com/example/saimumbarta/SplashScreen.java
@@ -0,0 +1,47 @@
+package com.example.saimumbarta;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window = getWindow() ;
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+
+
+        Thread splashTread = new Thread(){
+
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                super.run();
+            }
+        };
+
+        splashTread.start();
+
+
+
+
+    }
+
+
+}
